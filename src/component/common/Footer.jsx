0177678359daaf41ffdb4997503faeb6b1caf1ba
Pratiksha Karad <pratikshakarad735@gmail.com>
2025-02-10 23:59:49 +0530

    Initial frontend files

---
+++ src/component/common/Footer.jsx
@@ -0,0 +1,14 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                <p className="footertxt">Phegon Hotel | All Right Reserved &copy; {new Date().getFullYear()}</p>
+                
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
